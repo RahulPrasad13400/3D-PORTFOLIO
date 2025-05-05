bd100d7cef6a71ba4709f2d06919656c13ee4d48
RAHUL PRASAD <rahulprasad1399@gmail.com>
2025-05-05 22:30:13 +0530

    building header section

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+export default function App() {
+  return (
+    <section id='home' className="h-dvh relative text-white-50
+    px-5 md:p-0">
+      <div className="w-full h-full flex-center"> {/* FLEX-CENTER IS DEFINED IN THE INDEX.CSS FILE */}
+        <div className="container relative w-full h-full">
+          <div className="md:mt-40 mt-20">
+              <p className="font-medium md:text-2xl text-base">👋 Hey, I'm here</p>
+              <h1 className="font-bold md:text-9xl text-5xl">RAHUL PRASAD</h1>
+              <h1 className="font-bold md:text-9xl text-5xl">CREATIVE</h1>
+          </div>
+
+          <div className="">
+            <div className="flex justify-between items-end"> {/* ITEMS-END TO KEEP THE THINGS AT THE BOTTOM */}
+
+              <div className="">
+                <p>Explore</p>
+                <img src="images/arrowdown.svg" alt="arrow down" />
+              </div>
+
+              <div className="">
+                <img src="/images/shape.svg" alt="shape" />
+                <h1 className="font-bold md:text-9xl text-5xl">DEVELOPER</h1>
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
